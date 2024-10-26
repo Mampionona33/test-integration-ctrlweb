fbb486b49ab773744865e13af0ec647bc6772c23
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2024-10-26 10:37:59 +0000

    fix bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@ import HeloLight3 from "./HeloLight3.vue";
   <div class="relative flex flex-col md:h-full min-h-screen">
     <img
       src="/src/assets/Rectangle_9.png"
-      class="absolute top-0 left-0 object-cover pb-10 w-full h-screen md:h-fit"
+      class="absolute top-0 left-0 object-cover pb-10 w-full h-screen lg:h-fit"
     />
     <Navbar />
     <div
