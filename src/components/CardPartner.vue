7bfd83c306f34364614e47c568c6e8a2f58005a7
Mampionona33 <ramamps33@gmai.com>
2024-10-26 21:35:01 +0300

    finish partners

--- src/components/CardPartner.vue
+++ src/components/CardPartner.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    class="flex justify-center items-center flex-col p-4 gap-4 border border-slate-300"
+  >
     <h6>{{ title }}</h6>
     <div>
       <img :src="path" :alt="alt" />
--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -6,8 +6,37 @@
         Most calendars are designed for teams. Slate is designed for freelancers
       </p>
     </div>
-    <div>
+    <div
+      class="p-16 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:gap-0 md:grid-rows-2"
+    >
       <CardPartner title="Google" path="/src/assets/logos_google.svg" />
+      <CardPartner title="Amazon" path="/src/assets/amazon.png" />
+      <CardPartner title="Microsoft" path="/src/assets/microsoft.png" />
+      <CardPartner
+        title="Uber"
+        path="/src/assets/uber.svg"
+        class="hidden sm:block"
+      />
+      <CardPartner
+        title="Dropbox"
+        path="/src/assets/dropbox.svg"
+        class="hidden sm:block"
+      />
+      <CardPartner
+        title="Google"
+        path="/src/assets/logos_google.svg"
+        class="hidden sm:block"
+      />
+      <CardPartner
+        title="Amazon"
+        path="/src/assets/amazon.png"
+        class="hidden sm:block"
+      />
+      <CardPartner
+        title="Microsoft"
+        path="/src/assets/microsoft.png"
+        class="hidden sm:block"
+      />
     </div>
   </div>
 </template>
