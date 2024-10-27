5ef928ac0b5e2e5fb08d2e1aaacb918c0c8eef8d
Mampionona33 <ramamps33@gmai.com>
2024-10-27 06:40:21 +0300

    finish testimonials

--- src/components/testimonials.vue
+++ src/components/testimonials.vue
@@ -12,16 +12,16 @@ export default {
     class="text-center flex items-center justify-center py-20 flex-col gap-20 sm:w-full"
   >
     <h2 class="text-5xl">Testimonials</h2>
-    <div
-      class="flex justify-center gap-20 flex-col sm:w-full items-center md:w-full"
-    >
+    <div class="flex justify-center items-center gap-20 flex-col w-full">
       <div class="flex items-center justify-center sm:w-full">
         <img src="../assets/logos_ibm.svg" alt="logo_ibm" />
       </div>
-      <p class="flex w-1/2 text-center sm:w-full md:w-full">
-        Most calendars are designed for teams. Slate is designed for freelancers
-        who want a simple way to plan their schedule.
-      </p>
+      <div class="flex justify-center w-full items-center">
+        <p class="w-1/2 sm:w-full md:w-full">
+          Most calendars are designed for teams. Slate is designed for
+          freelancers who want a simple way to plan their schedule.
+        </p>
+      </div>
     </div>
     <div class="flex flex-row gap-4">
       <div class="flex items-center justify-center">
