3304f9ff04a408f993dcd12f58b52916ab3ae968
Mampionona33 <ramamps33@gmai.com>
2024-10-28 21:28:11 +0300

    mise en place formulaire contact us

---
+++ src/components/FormContactUs.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppInput from "./appInput.vue";
+</script>
+
+<template>
+  <div>
+    <h3>Contact Us</h3>
+    <form
+      action=""
+      method="post"
+      class="flex flex-col items-center justify-center gap-8"
+    >
+      <div>
+        <AppInput type="text" placeholder="Your Name" />
+      </div>
+      <div>
+        <AppInput type="email" placeholder="Your Email" />
+      </div>
+    </form>
+  </div>
+</template>
--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,39 +1,107 @@
 <template>
-  <div>
-    <div>
-      <h2>Contact Us</h2>
-      <h4>Most calendars are designed for teams.</h4>
+  <div class="p-20 flex flex-col gap-8">
+    <div class="flex gap-8 flex-col">
+      <h2 class="text-5xl text-slate-800 text-center">Contact Us</h2>
+      <h4 class="text-3xl text-slate-600 text-center">
+        Most calendars are designed for teams.
+      </h4>
     </div>
+
+    <!-- Section des icônes de réseaux sociaux -->
     <div class="flex gap-4 flex-row text-2xl items-center justify-center">
       <div v-for="icon in listIcons" :key="icon.name">
         <router-link
           :to="icon.link"
-          class="text-2xl hover:text-blue-300"
-          :class="[icon.color, 'transition-colors']"
+          :class="[icon.color, icon.hoverColor, 'transition-colors']"
         >
           <font-awesome-icon :icon="['fab', icon.name]" class="text-2xl" />
         </router-link>
       </div>
     </div>
+
+    <!-- Section des informations de contact -->
+    <div class="flex gap-4 flex-col text-2xl items-start justify-center mt-6">
+      <div
+        v-for="info in infos"
+        :key="info.id"
+        class="flex items-center space-x-2"
+      >
+        <font-awesome-icon
+          :icon="['fas', info.icon]"
+          :class="[
+            info.color,
+            info.hoverColor,
+            'transition-colors',
+            'text-2xl',
+          ]"
+        />
+        <h2 class="text-xl">{{ info.title }}</h2>
+      </div>
+    </div>
+
+    <!-- Formulaire -->
+    <FormContactUs />
   </div>
 </template>
 
 <script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faLocationDot,
+  faEnvelope,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import FormContactUs from "./FormContactUs.vue";
+
+library.add(faLocationDot, faEnvelope, faPhone);
+
+// Liste des icônes des réseaux sociaux
 const listIcons = [
   {
     name: "twitter",
     link: "https://www.twitter.com",
     color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
   },
   {
     name: "square-facebook",
     link: "https://www.facebook.com",
     color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
   },
   {
     name: "linkedin",
     link: "https://www.linkedin.com",
     color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
+  },
+];
+
+// Informations de contact avec les icônes et couleurs de survol
+const infos = [
+  {
+    id: "1",
+    icon: "location-dot",
+    title: "6386 Spring St undefined Anchorage, Georgia 12473 United States",
+    color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
+  },
+  {
+    id: "2",
+    icon: "phone",
+    title: "(843) 555-0130",
+    color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
+  },
+  {
+    id: "3",
+    icon: "envelope",
+    title: "Main Avenue, 987",
+    color: "text-blue-500",
+    hoverColor: "hover:text-blue-300",
   },
 ];
 </script>
+
+<style scoped></style>
