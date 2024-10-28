664c74e8aaca1bcdac7276438a95e60f997c77ee
Mampionona33 <ramamps33@gmai.com>
2024-10-29 02:21:57 +0300

    mise en place icon footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -31,10 +31,28 @@ const items = [
     ],
   },
 ];
+const infos = [
+  {
+    id: "1",
+    icon: "location-dot",
+    title: "6386 Spring St undefined Anchorage, Georgia 12473 United States",
+    color: "text-white",
+    hoverColor: "hover:text-blue-300",
+  },
+  {
+    id: "2",
+    icon: "phone",
+    title: "(843) 555-0130",
+    color: "text-white",
+    hoverColor: "hover:text-blue-300",
+  },
+];
+
 export default {
   data() {
     return {
       items,
+      infos,
     };
   },
 };
@@ -42,13 +60,31 @@ export default {
 
 <template>
   <div
-    class="bg-[#252B42] text-white flex flex-col gap-20 items-center text-center py-20"
+    class="bg-[#252B42] text-white items-center text-center py-20 gap-20 flex flex-col"
   >
-    <div v-for="(item, index) in items" :key="index">
-      <h3 class="font-bold text-xl">{{ item.title }}</h3>
-      <ul class="flex flex-col gap-4">
-        <li v-for="link in item.links" :key="link">{{ link }}</li>
-      </ul>
+    <div class="flex flex-col gap-20">
+      <div v-for="(item, index) in items" :key="index">
+        <h3 class="font-bold text-xl">{{ item.title }}</h3>
+        <ul class="flex flex-col gap-4">
+          <li v-for="link in item.links" :key="link">{{ link }}</li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <div>
+        <div v-for="info in infos" :key="info.id" class="flex gap-4">
+          <font-awesome-icon
+            :icon="['fas', info.icon]"
+            :class="[
+              info.color,
+              info.hoverColor,
+              'transition-colors',
+              'text-2xl',
+            ]"
+          />
+          <h2 class="text-base text-white">{{ info.title }}</h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>
